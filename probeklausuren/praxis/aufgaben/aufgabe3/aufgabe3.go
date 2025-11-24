d384d5aa87d472b1ece1af51f0277af0dda9419e
peterv1002 <peter.voetterl@gmail.com>
2025-11-24 18:12:43 +0100

    ugh

--- probeklausuren/praxis/aufgaben/aufgabe3/aufgabe3.go
+++ probeklausuren/praxis/aufgaben/aufgabe3/aufgabe3.go
@@ -8,6 +8,13 @@ ZUSATZBEDINGUNG: Die Funktion muss rekursiv sein.
 
 // CountOdd erwartet eine Liste von Zahlen und liefert die Anzahl der ungeraden Zahlen darin.
 func CountOdd(list []int) int {
-	// TODO
-	return 0
+
+	if len(list) == 0 {
+		return 0
+	}
+	count := CountOdd(list[1:])
+	if list[0]%2 != 0 {
+		count++
+	}
+	return count
 }
